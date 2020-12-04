ef99f28c55d024a3039c27153996262f3f7ae059
sakamoto shinji <shinj0712.dev@gmail.com>
2020-12-04 17:23:07 +0900

    開発環境構築前に作成していたフロント部分をまとめて追加

---
+++ src/resources/js/components/modules/ImageComponent.vue
@@ -0,0 +1,45 @@
+/**
+ * コンポーネントの使用例
+ * <image-component imageUrl="../image/group-photo_toutoSpring2017.jpg" alt="2017年東都春季リーグ集合写真"/>
+ * pcスタイル未設定
+*/
+
+<template>
+<div class="c-image">
+  <img class="c-image__image" :src="imageUrl" :alt="alt" :class="radiusClass">
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageUrl: String,
+    alt: String,
+    radius: {
+      type: Boolean,
+      default: false
+    },
+  },
+  computed: {
+    radiusClass() {
+      return (this.radius) ? `c-image__image-radius` : null;
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.c-image {
+  width: 100%;
+
+  &__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    &-radius {
+      border-radius: radius(soft);
+    }
+  }
+}
+</style>
---
+++ src/resources/js/components/modules/MenuPanelComponent.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="c-menuPanel">
+    <image-component class="c-menuPanel__image" :imageUrl="panelImageUrl" :alt="imageAlt" :radius="imageRadius"/>
+    <view-all-button-component class="c-menuPanel__button" :name="buttonName" :buttonSize="buttonSize"/>
+  </div>
+</template>
+
+<script>
+// component import
+import ImageComponent from './ImageComponent';
+import ViewAllButtonComponent from './button/ViewAllButtonComponent';
+
+export default {
+  components: {
+    ImageComponent,
+    ViewAllButtonComponent,
+  },
+  props: {
+    buttonName: String,
+    panelImageUrl: String,
+    imageAlt: String,
+    buttonSize: String,
+  },
+  data() {
+    return {
+      imageRadius: true,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.c-menuPanel {
+
+  &__image {
+    width: 100%;
+    height: 250px;
+    opacity: 0.7;
+    box-shadow: 0 0 8px 2px color(shadow);
+
+    // pc style
+    @include mq(md) {
+      height: 200px;
+    };
+  }
+
+  &__button {
+    transform: translateY(-120%);
+
+    // pc style
+    @include mq(md) {
+      transform: translateY(-50%);
+    }
+  }
+}
+</style>
