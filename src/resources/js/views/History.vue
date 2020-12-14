b622e5bb8826738ef20f8e46ffc5d47dda67791a
sakamoto shinji <shinj0712.dev@gmail.com>
2020-12-14 17:40:58 +0900

    ContentsTitleを表示とページの土台を作成

--- src/resources/js/views/History.vue
+++ src/resources/js/views/History.vue
@@ -1,15 +1,31 @@
 <template>
-<div class="wrap">
-  history
+<div class="history">
+
+  <section class="history__history section-container">
+    <contents-title-component :title="messages.History.Title.Main" :subTitle="messages.History.Title.Sub"/>
+
+
+  </section>
+
 </div>
 </template>
 
 <script>
-export default {
+// import components
+import ContentsTitleComponent from '../components/modules/ContentsTitleComponent'
 
+export default {
+  components: {
+    ContentsTitleComponent
+  },
+  props: {
+
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.history {
 
+}
 </style>
