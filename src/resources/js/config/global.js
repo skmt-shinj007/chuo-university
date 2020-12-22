a94611c1f3b8ae1cc47188b53d2c18fab3c4f842
sakamoto shinji <shinj0712.dev@gmail.com>
2020-12-22 17:31:11 +0900

    スクロール量の取得をグローバルに使えるように追加

--- src/resources/js/config/global.js
+++ src/resources/js/config/global.js
@@ -12,19 +12,59 @@ export default {
       tabletWidth: 560,
       pcWidth: 992,
       windowWidth: null,
+      scrollAmount: null,
     }
   },
 
   created() {
+
+    // ウインドウ幅を取得
     this.windowWidth = window.innerWidth;
     window.addEventListener('resize', () => {
       this.windowWidth = window.innerWidth;
     });
   },
 
+  mounted() {
+
+    // スクロール量を取得
+    this.scrollAmount = window.scrollY;
+    window.addEventListener('scroll', () => {
+      this.scrollAmount = window.scrollY;
+    });
+  },
+
   beforeDestroy() {
+
+    // コンポーネント破棄直前に追加したイベントをリスナーから削除
     window.removeEventListener('resize', () => {
       this.windowWidth = window.innerWidth;
     });
+    window.removeEventListener('scroll', () => {
+      this.scrollAmount = window.scrollY;
+    });
+  },
+
+  /**
+   * グローバルで使用するメソッドを定義
+   * コンポーネントからのアクセス：this.method()
+   */
+  methods: {
+    getElements() {  // https://infoteck-life.com/a0186-js-dom-id-multiple/
+      let elm = {};
+      let elements = {};  // 一括でidを取得したデータが格納される:Object
+      let id = '';        // id名が入る:String
+
+      for (let i = 0; i < arguments.length; i++) {
+        id = arguments[i];
+        elm = document.getElementById(id);
+
+        if (elm != null) {
+          elements[id] = elm;
+        }
+      }
+
+      return elements;
+    }
   },
 }
