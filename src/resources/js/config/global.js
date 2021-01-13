b482ef2ce30a42834df6e89175fcc89d3e5f391a
sakamoto shinji <shinj0712.dev@gmail.com>
2021-01-13 14:31:48 +0900

    ユーザー情報統合に伴って取得処理を修正

--- src/resources/js/config/global.js
+++ src/resources/js/config/global.js
@@ -13,6 +13,20 @@ export default {
       pcWidth: 992,
       windowWidth: null,
       scrollAmount: null,
+
+      /**
+     * ユーザーカテゴリー情報
+     * 1. 選手
+     * 2. スタッフ
+     * 3. OB（役職あり）
+     * 4. 現役OB（活躍中）
+     * 5. OB
+     */
+      playerNum: 1,
+      staffNum: 2,
+      officerNum: 3,
+      activeAlumniNum: 4,
+      alumniNum: 5
     }
   },
 
