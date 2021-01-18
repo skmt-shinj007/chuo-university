34c283dc59f7763c6ae8d4ea16c218ce5ddefe72
sakamoto shinji <shinj0712.dev@gmail.com>
2021-01-18 18:13:48 +0900

    連絡先タイトルにリンク追加

--- src/resources/js/components/layouts/FooterComponent.vue
+++ src/resources/js/components/layouts/FooterComponent.vue
@@ -48,7 +48,9 @@
       <div class="footer__information-logo"/>
 
       <address>
-        <h4 class="footer__information-title">{{ messages.Information.ClubName }}</h4>
+        <h4 class="footer__information-title">
+          <router-link to="/">{{ messages.Information.ClubName }}</router-link>
+        </h4>
         <span class="footer__information-item">{{ messages.Information.Address }}</span>
         <a class="footer__information-telephone" :href="`tel:+${telephoneNum}`">
           {{ messages.Information.TelephoneNumber }}
@@ -278,18 +280,18 @@ export default {
   }
 
   &__information-logo {
-    display: none;
+    pointer-events: none;
+    width: interval(18);
+    height: 100%;
+    opacity: .2;
+    @include background-image('/svg/chuo-logo-white.svg');
+    position: absolute;
+    top: 50%;
+    left: interval(2);
+    transform: translateY(-50%);
 
     @include mq(sm) {
-      display: block;
       width: interval(20);
-      height: 100%;
-      opacity: .2;
-      @include background-image('/svg/chuo-logo-white.svg');
-      position: absolute;
-      top: 50%;
-      left: interval(2);
-      transform: translateY(-50%);
     }
   }
 
