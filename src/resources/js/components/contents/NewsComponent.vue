fffdd4b7f96adfbbd06ecb341398f3cf8d9acce5
sakamoto shinji <shinj0712.dev@gmail.com>
2021-01-26 17:05:40 +0900

    ニュースセクション修正

--- src/resources/js/components/contents/NewsComponent.vue
+++ src/resources/js/components/contents/NewsComponent.vue
@@ -1,29 +1,29 @@
 <template>
 <div class="news">
-  <!-- :color="white" 記述でテキストを白に -->
-  <contents-title-component :title="contentsTitle" :subTitle="contentsSubTitle"/>
 
-  <div class="news__list">
-    <text-list-component/>
-  </div>
+  <contents-title-component :title="messages.SectionTitles.News.Main" :subTitle="messages.SectionTitles.News.Sub"/>
+
+  <ul class="news-list">
+    <li class="news-list__item">
+      <a class="news-list__link" href="#">ここにニュースが入ります。ここにニュースが入ります。ここにニュースが入ります。ここにニュースが入ります。</a>
+    </li>
+    <li class="news-list__item">
+      <a class="news-list__link" href="#">ここにニュースが入ります。ここにニュースが入ります。</a>
+    </li>
+    <li class="news-list__item">
+      <a class="news-list__link" href="#">ここにニュースが入ります。ここにニュースが入ります。</a>
+    </li>
+  </ul>
 </div>
 </template>
 
 <script>
 // components import
 import ContentsTitleComponent from '../modules/ContentsTitleComponent';
-import TextListComponent from '../modules/TextListComponent.vue';
 
 export default {
   components: {
     ContentsTitleComponent,
-    TextListComponent,
-  },
-  data() {
-    return {
-      contentsTitle: 'news',
-      contentsSubTitle: 'ニュース',
-    }
   },
 }
 </script>
@@ -38,8 +38,31 @@ export default {
     padding: interval(3);
   }
 
-  &__list {
-    margin: 0 interval(2);
+  &-list {
+    width: 100%;
+    list-style: disc;
+    padding-left: interval(3);
+
+    &__item {
+      font-size: font(12);
+      margin-bottom: interval(1);
+      word-wrap: break-word;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+
+    &__link {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+
+      @include hover {
+        text-decoration: underline;
+      }
+    }
   }
 }
 </style>
