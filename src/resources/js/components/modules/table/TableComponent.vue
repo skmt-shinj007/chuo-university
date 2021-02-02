2d0e8a65c5ccd82361cf7353040c376070f8edf9
sakamoto shinji <shinj0712.dev@gmail.com>
2021-02-02 11:37:16 +0900

    スタッフチケットのモーダルを作成

--- src/resources/js/components/modules/table/TableComponent.vue
+++ src/resources/js/components/modules/table/TableComponent.vue
@@ -2,44 +2,99 @@
 <!-- ※ オブジェクトの中身は、「key」と「value」で構成し、タイトルになるカラムには key の値が入る。 -->
 <template>
   <table class="common-table">
-    <tr class="common-table__record" :class="sizing" v-for="(tableItem, n) in tableItems" :key="n">
+    <tr class="common-table__record" :class="sizing" v-for="(tableItem, n) in levelingItems" :key="n">
       <th class="common-table__key">{{ tableItem.key }}{{ addKeyText }}</th>
-      <td class="common-table__value" :class="bgClassChange">{{ tableItem.value }}{{ addValueText }}</td>
+      <td class="common-table__value" :class="background">{{ tableItem.value }}{{ addValueText }}</td>
     </tr>
   </table>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      /**
+       * 整地された tableItems が入る
+       */
+      levelingItems: [],
+    }
+  },
+
   props: {
+    /**
+     * テーブルを作成する配列
+     */
     tableItems: {
       type: Array,
       default: []
     },
+
+    /**
+     * key や value に追加で付与したい文字列
+     * 主に「単位」など
+     */
     addKeyText: {
       type: String,
       default: ''
     },
+
     addValueText: {
       type: String,
       default: ''
     },
+
+    /**
+     * テーブルの背景を透明にするかどうかを判定するフラグ
+     */
     valueTransparent: {
       type: Boolean,
       default: false
     },
+
+    /**
+     * テーブルのフォントサイズを指定する
+     */
     size: {
       type: String,
       default: null
     }
   },
+
   computed: {
-    bgClassChange() {
+    /**
+     * 背景を透過にするクラスを付与
+     */
+    background() {
       return (this.valueTransparent) ? `common-table__value--transparent` : null;
     },
+
+    /**
+     * サイズによってクラスを付与
+     */
     sizing() {
       return (this.size) ? `common-table__record--${this.size}` : null;
+    },
+
+    /**
+     * 配列を整地する
+     */
+    leveling() {
+      this.tableItems = this.tableItems
     }
+  },
+
+  beforeMount() {
+    /**
+     * 1.value値がnullの要素
+     * 2.value値に「null」という文字列が入っている要素
+     * 上記をフィルターした配列を[levelingItems]に代入する。
+     */
+    this.levelingItems = this.tableItems.filter(element => {
+      if (typeof element.value === 'string') {
+        if(element.value.indexOf('null') !== -1) element.value = null;
+      }
+      return (element.value !== null);
+    });
   },
 }
 </script>
