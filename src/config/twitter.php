5b5353d88e12c444369704f3bbc5ab0c00853ea8
sakamoto shinji <shinj0712.dev@gmail.com>
2021-02-19 23:32:27 +0900

    各キーを保存しておくphpファイル追加

--- src/config/twitter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-  'api_key' => env('TWITTER_API_KEY', ''),
-  'api_key_secret' => env('TWITTER_API_KEY_SECRET', ''),
-  'access_token' => env('TWITTER_API_ACCESS_TOKEN', ''),
-  'access_token_secret' => env('TWITTER_API_ACCESS_TOKEN_SECRET', ''),
-  'callback_url' => env('TWITTER_CLIENT_CALLBACK', ''),
-];
