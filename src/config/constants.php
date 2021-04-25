551471dcf39d6c691a557b4d3c6444af4fa269c7
sakamoto shinji <shinj0712.dev@gmail.com>
2021-04-25 22:21:04 +0900

    定数格納構造の修正

--- src/config/constants.php
+++ src/config/constants.php
@@ -1,15 +1,16 @@
 <?php
 return [
-  /**
-   * [プロバイダーのTwitterユーザーID]
-   * ユーザーを追加したい場合、ここにIDを追加する。
-   */
-  'providers' => [
-    'twitter_user_id' => [
-      'HaNa' => '4824839654',
-    ]
-  ],
+  'twitter' => [
+    'baseUrl' => 'https://twitter.com/',
 
+    /**
+     * [プロバイダーのTwitterユーザーID]
+     * ユーザーを追加したい場合、ここにIDを追加する。
+     */
+    'providers' => [
+      'HaNa' => '4824839654',
+    ],
+  ],
 ]
 
 ?>
