885ff19ac8942f43f5166a7f396947284f5e6c91
juuun-jp <juuun.2020@gmail.com>
2021-05-03 13:00:24 +0900

    中間テーブルのテーブルカラム、シーダー修正

--- src/database/migrations/2021_03_13_063526_create_user_tags_table.php
+++ src/database/migrations/2021_03_13_063526_create_user_tags_table.php
@@ -14,12 +14,10 @@ class CreateUserTagsTable extends Migration
     public function up()
     {
         Schema::create('user_tags', function (Blueprint $table) {
-            $table->id()->comment('primarykey');
+            // $table->id()->comment('primarykey');
             $table->foreignId('user_id')->comment('ユーザーID');
             $table->foreignId('tag_id')->comment('タグID');
-            $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
-            $table->softDeletes();
+            $table->primary(['user_id', 'tag_id']);
 
             // 外部キー制約
             $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
