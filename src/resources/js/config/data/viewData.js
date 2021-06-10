5f4104c552e8c8f13141f5f50bac8e6483e9098c
sakamoto shinji <shinj0712.dev@gmail.com>
2021-06-10 22:56:25 +0900

    homeViewDataのデータ移行

--- src/resources/js/config/data/viewData.js
+++ src/resources/js/config/data/viewData.js
@@ -2,7 +2,41 @@
  * サイト内で使用するコンテンツのデータを定義します。
  * @exports {Object} viewData
  */
+import messages from '../messages.json';
 const viewData = {
+
+  about: [
+    {
+      title: messages.SectionTitles.Team,
+      img: { src: "group01_2017", alt: "2017年東都春季リーグ戦" },
+      button: messages.Links.Member,
+      text: messages.about.team,
+    },
+    {
+      title: messages.SectionTitles.Hakumonkai,
+      img: { src: "hakumon01", alt: "中央大学多摩キャンパス白門" },
+      button: messages.Links.Hakumonkai,
+      text: messages.about.hakumonkai,
+    },
+    {
+      title: messages.SectionTitles.Result,
+      img: { src: "group01_2018", alt: "2018年東都春季リーグ" },
+      button: messages.Links.TournamentResult,
+      text: messages.about.result,
+    },
+    {
+      title: messages.SectionTitles.Activity,
+      img: { src: "activity", alt: "中央大学ソフトテニスコート" },
+      button: messages.Links.Activity,
+      text: messages.about.activity,
+    },
+    {
+      title: messages.SectionTitles.Photo,
+      img: { src: "group01_2019", alt: "2019春合宿" },
+      button: messages.Links.Photo,
+      text: messages.about.photo,
+    }
+  ],
 
   officersTableColumn: [
     { id: 1, prop: 'main_officer_tag_name', label: "白門会役職" },
@@ -29,6 +63,6 @@ const viewData = {
     director: 21,
     auditor: 22,
     counsellor: 23
-  }
+  },
 }
 export default viewData;
